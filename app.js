332c1cad4fc1855efa2d77cd76166e129b3d4d75
B1-sauce <hby940604@gmail.com>
2021-05-15 12:45:08 -0700

    update

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017/test1', { useNewUrlParser: true }, {
 const db = mongoose.connection;
 
 router.get('/loaderio-ea15dffc4acf1a237bf998540eb652cb/', (req, res) => {
-  res.send(path.join(__dirname, './loaderio-ea15dffc4acf1a237bf998540eb652cb.txt'));
+  res.send('loaderio-ea15dffc4acf1a237bf998540eb652cb.txt');
 })
 
 router.get('/products', (req, res) => {
